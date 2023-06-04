c5e8b0d4cd3d20d9bc09750cf5238a72d076216b
Timothy Lickteig <timothylickteig@Timothys-Mac-mini.local>
2023-06-04 11:07:04 -0500

    Initial project commit

---
+++ MinePaper/MinePaper/MinePaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinePaperApp.swift
+//  MinePaper
+//
+//  Created by Timothy Lickteig on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MinePaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
